de65b7d85aa4e877abd2d98d204518609522a2f7
MacEverett <maceverett3@gmail.com>
2016-08-02 11:45:18 -0700

    Update main.js
    ES2015

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ var url = "https://api.nytimes.com/svc/topstories/v2/"+ selection +".json";
 // if (data.results.length === 0) {
 //     $('.news-articles').append(`<p>Sorry. Please try another section.</p>`);
 // } else {
-//     let nytData = data.results.filter(function(item) {
+     let nytData = data.results.filter(function(item) {
 //         return item.multimedia.length;
 //     }).splice(0, 12)
 //     .forEach(function(item, index) {
@@ -47,7 +47,7 @@ var url = "https://api.nytimes.com/svc/topstories/v2/"+ selection +".json";
 //         </div>
 //         </a>`);
 // // TO MAKE SURE ARTICLE HAS IMAGE
-// let img = item.multimedia[4];
+let img = item.multimedia[4];
 // $('.article-' + index).css('background-image', `url("${img.url}")`);
 // $('.text-'+index).hide();
 // $('.article-'+index).hover(function() {
